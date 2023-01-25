5a53454b2b43796b01ca39209bed688265372d9d
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-01-25 09:18:25 +0200

    debug

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pylab
 
 frame = pd.read_csv("titles.csv")
 movie_frame = frame[frame.type == 'MOVIE']
@@ -8,10 +10,9 @@ show_frame = frame[frame.type == 'SHOW']
 movie_imdb = movie_frame.loc[:,'imdb_score']
 show_imdb = show_frame.loc[:,'imdb_score']
 
-movie_normalized_df=(movie_imdb - movie_imdb.min())/(movie_imdb.max() - movie_imdb.min())
-show_normalized_df=(show_imdb - movie_imdb.min())/(movie_imdb.max() - movie_imdb.min())
 
-fig, axes = plt.subplots(1, 2)
-movie_normalized_df.hist(ax = axes[0])
-show_normalized_df.hist(ax = axes[1])
+plt.subplot(2, 2, 1)
+plt.hist(movie_imdb, np.arange(0,10,0.2))
+plt.subplot(2, 2, 2)
+plt.hist(show_imdb, np.arange(0,10,0.2))
 plt.show()
